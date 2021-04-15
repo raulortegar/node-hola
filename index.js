43b8f22fc06385c0f12dab7992a907bf5e0229e0
raulortegar <raulortegarcia@gmail.com>
2021-04-16 00:52:23 +0200

    const por var

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-var express = require('express');
+const express = require('express');
 
-var app = express();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('<h1>hola clase</h1>');
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
 
 module.exports = app;
