bcfe6239791d6e5cc8ee7915af82966bebb0810e
raulortegar <raulortegarcia@gmail.com>
2021-04-20 00:15:08 +0200

    Build and Test

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 
 const app = express();
+<<<<<<< Updated upstream
 
 PORT = process.env.PORT || 3000;
+=======
+const PORT =  process.env.PORT || 3000;
+>>>>>>> Stashed changes
 
 app.get('/', function (req, res) {
   res.send('<h1>hola clase</h1>');
 });
 
 app.listen(PORT, () => {
+<<<<<<< Updated upstream
   console.log(`Servidor a la escucha en puerto: ${PORT}...`);
 });
+=======
+  console.log(`Servidor escuchando en puerto: ${PORT} ...`);
+});
+
+
+>>>>>>> Stashed changes
 
 module.exports = app;
